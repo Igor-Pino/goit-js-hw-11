b49f3f4a011399c9953653a7c00350e568cabb50
Igor-Pino <igorpinyo@gmail.com>
2021-09-08 15:49:25 +0300

    12

--- src/partials/js_templates/templateMarkup.js
+++
@@ -1,9 +0,0 @@
-import templetConstructor from '../templates/template.hbs';
-import menuItem from '../../menu.json';
-
-
-const menuRef = document.querySelector('.js-menu')
-
-const markup = templetConstructor(menuItem)
-
-menuRef.insertAdjacentHTML('afterbegin', markup)
